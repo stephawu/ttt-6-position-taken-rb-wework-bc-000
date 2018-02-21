ef9142933de24bfebb805b900388c386cf8942d9
stephanie wu <stephanie.wu@wework.com>
2018-02-21 01:44:55 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,13 +1,15 @@
 # code your #position_taken? method here!
+taken = nil
 
 def position_taken?(board, index)
 
-if board[index] || "" || " " || nil
-  then false
+if (board[index] == "" || (board[index] == " " ||(board[index] == nil)
+  taken = false
+
+else board[index] == "X" || "O"
+  taken = true
 end
 
-elseif board[index] == "X" || "O"
-  then true
 end
 
-end
+position_taken?
