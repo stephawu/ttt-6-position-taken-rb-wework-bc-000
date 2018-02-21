55b9a2663368aa8909cedacb1b59758de4d09592
stephanie wu <stephanie.wu@wework.com>
2018-02-21 01:48:36 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 taken = nil
 
 def position_taken?(board, index)
- if (board[index] == "" || (board[index] == " " ||(board[index] == nil)
+ if (board[index] == "" || board[index] == " " || board[index] == nil)
   taken = false
 
  else board[index] == "X" || "O"
