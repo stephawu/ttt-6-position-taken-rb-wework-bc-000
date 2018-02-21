dd3671d9ed4eadb185d2392bceb5e3478aea7d91
stephanie wu <stephanie.wu@wework.com>
2018-02-21 01:38:01 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+
+if board[index] || "" || " " || nil
+  then false
+end
+
+elseif board[index] == "X" || "O"
+  then true
+end
+
+end
